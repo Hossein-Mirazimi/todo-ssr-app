5efe7c5097b9cc7609be5ebf30fc80c585da65e5
Hossein Mirazimi <h.mirazimi@alibaba.ir>
2022-05-19 17:31:02 +0430

    chore: update windicss configs

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,14 +1,9 @@
 <template>
-  <h1 class="text-4xl my-14 text-bold">
-    Vite SSR Starter
-  </h1>
-  <button class="px-3 py-2 text-white bg-gray-500 rounded-md focus:ring" @click="state.count++">
-    count is: {{ state.count }}
-  </button>
+  <header>
+    <div class="flex flex-row">
+      <button class="default-focus p-4 font-medium border-transparent hover:border-gradient2-button transition-all duration-200 ease-in-out bg-opacity-50 border-b-2 flex items-center">
+        Sign up
+      </button>
+    </div>
+  </header>
 </template>
-
-<script setup lang="ts">
-import { reactive } from 'vue'
-
-const state = reactive({ count: 0 })
-</script>
--- windi.config.ts
+++ windi.config.ts
@@ -1,37 +1,65 @@
 import { defineConfig } from 'vite-plugin-windicss'
-import colors from 'windicss/colors'
 import defaultTheme from 'windicss/defaultTheme'
 
 const config = defineConfig({
+  darkMode: false,
   theme: {
     extend: {
       fontFamily: {
-        sans: ['"Helvetica Neue"', 'Arial', '"Hiragino Kaku Gothic ProN"', '"Hiragino Sans"', 'Meiryo', ...defaultTheme.fontFamily.sans],
+        sans: ['Inter', '"Helvetica Neue"', 'Arial', '"Hiragino Kaku Gothic ProN"', '"Hiragino Sans"', 'Meiryo', ...defaultTheme.fontFamily.sans],
       },
       colors: {
-        teal: colors.teal,
+        'primary-default': '#FC76A1',
+        'primary-yellow': '#DBBE56',
+        'primary-orange': '#E39264',
+        'primary-red': '#D25A61',
+        'primary-purple': '#AE68E6',
+        'primary-blue': '#4BACD5',
+        'primary-teal': '#70C4BF',
+        'primary-gray': '#7B7B8E',
+        'primary-brown': '#9E7F72',
+        'primary-darkBrown': '#5B4D47',
+        'primary-background': '#181820',
+        'secondary-background': '#1E1E29',
+        'hero-background': '#1D1D26',
+        'navigation': '#1D1D27',
+        'primary-card': '#21212B',
+        'secondary-card': '#343343',
+        'hover-card': '#272732',
+        'gray-text': '#C3C3D9',
+        'gray-button': '#30303D',
+        'gradient1-button': '#BB3FDD',
+        'gradient2-button': '#FB6DA9',
+        'gradient3-button': '#FF9F7C',
       },
-      opacity: {
-        15: '0.15',
-        20: '0.2',
-        95: '0.95',
+      borderWidth: {
+        1: '1px',
+        3: '3px',
       },
-      margin: {
-        96: '24rem',
-        128: '32rem',
+      fontSize: {
+        xxs: '.625rem',
       },
-      padding: {
-        '2/3': '66.666%',
+      boxShadow: {
+        button: 'inset 0 -4px 0px 0 rgba(0, 0, 0, 0.25)',
       },
       height: {
-        18: '4.5rem',
-      },
-      width: {
-        18: '4.5rem',
+        '90vh': '90vh',
+        '80vh': '80vh',
       },
     },
   },
-  darkMode: 'class',
+  extract: {
+    // accepts globs and file paths relative to project root
+    include: [
+      'index.html',
+      'src/**/*.{vue,html,css,scss}',
+    ],
+    exclude: [
+      'node_modules/**/*',
+      '.git/**/*',
+      'src/assets/**/*',
+    ],
+  },
 })
 
 export default config
