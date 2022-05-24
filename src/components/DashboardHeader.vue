7e904f26a29b63f1b343c680465fd5b95929bf69
Hossein Mirazimi <h.mirazimi@alibaba.ir>
2022-05-24 21:21:16 +0430

    chore: add dashboard header component in mobile and desktop

---
+++ src/components/DashboardHeader.vue
@@ -0,0 +1,81 @@
+<template>
+  <header class="items-center justify-center hidden text-white shadow-md bg-navigation font-regular md:flex">
+    <div class="flex flex-row items-center justify-between w-full px-5">
+      <div>
+        <nav class="flex items-center">
+          <a
+            class="default-focus py-4 font-medium border-transparent opacity-70 border-b-2 hover:opacity-100 flex items-center border-white opacity-100"
+            href="/app"
+          >
+            <svg
+              stroke="currentColor"
+              fill="currentColor"
+              stroke-width="0"
+              viewBox="0 0 24 24"
+              class="mr-2 text-2xl text-white"
+              height="1em"
+              width="1em"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path fill="none" d="M0 0h24v24H0z"></path>
+              <path d="M3 13h8V3H3v10zm0 8h8v-6H3v6zm10 0h8V11h-8v10zm0-18v6h8V3h-8z"></path>
+            </svg>Dashboard</a>
+        </nav>
+      </div>
+      <div class="relative inline-block text-left">
+        <button @click="isShow = !isShow" class="rounded-full default-focus">
+          <Avatar src="https://i.pravatar.cc/150?img=36" w="35px" h="35px"/>
+        </button>
+        <div
+          class="origin-top-right absolute right-0 z-20 mt-2 w-56 rounded-md shadow-lg bg-secondary-background ring-1 ring-black ring-opacity-5 divide-y divide-primary-background"
+          :class="isShow ? 'visible' : 'invisible'"
+          role="menu"
+          aria-orientation="vertical"
+          aria-labelledby="options-menu"
+        >
+          <div class="py-1">
+            <p class="block px-4 pt-2 pb-1 text-sm text-gray-200" role="menuitem">
+              Signed in as
+            </p>
+            <p class="inline-flex items-start px-4 pb-2 text-sm font-semibold text-gray-200 just" role="menuitem">
+              Hossein Mirazimi
+              <span class="px-1 ml-2 font-normal tracking-wide text-purple-100 uppercase rounded-md bg-primary-default border-primary-default border-1 bg-opacity-60 text-xxs">
+                Admin
+              </span>
+            </p>
+          </div>
+          <div class="py-1">
+            <a
+              href="#"
+              class="block px-4 py-2 text-sm text-gray-500 pointer-events-none hover:bg-secondary-card"
+              role="menuitem"
+            >
+              Account settings
+            </a>
+          </div>
+          <div class="py-1">
+            <button
+              class="block w-full px-4 py-2 text-sm text-left text-gray-200 hover:bg-secondary-card"
+              role="menuitem"
+            >
+              Sign out
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  setup() {
+    const isShow = ref(false)
+    return {
+      isShow,
+    }
+  },
+})
+</script>
---
+++ src/components/DashboardHeaderMobile.vue
@@ -0,0 +1,55 @@
+<template>
+  <header
+    id="mobile_menu"
+    class="fixed bottom-0 z-20 flex items-start justify-center w-full text-sm font-medium text-white bg-secondary-background md:hidden"
+  >
+    <div class="flex flex-row items-center justify-between w-full">
+      <nav class="flex items-center justify-around w-full">
+        <a
+          class="default-focus py-4 opacity-70 hover:opacity-100 flex items-center opacity-100"
+          href="/app"
+        >
+          <svg
+            stroke="currentColor"
+            fill="currentColor"
+            stroke-width="0"
+            viewBox="0 0 24 24"
+            class="mr-2 text-2xl text-white"
+            height="1em"
+            width="1em"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path fill="none" d="M0 0h24v24H0z"></path>
+            <path d="M3 13h8V3H3v10zm0 8h8v-6H3v6zm10 0h8V11h-8v10zm0-18v6h8V3h-8z"></path>
+          </svg>
+          Dashboard
+        </a>
+        <a class="default-focus py-4 opacity-70 hover:opacity-100 flex items-center " href="/">
+          <svg
+            stroke="currentColor"
+            fill="currentColor"
+            stroke-width="0"
+            viewBox="0 0 24 24"
+            class="mr-2 text-2xl "
+            height="1em"
+            width="1em"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path fill="none" d="M0 0h24v24H0z"></path>
+            <path
+              d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 3c1.66 0 3 1.34 3 3s-1.34 3-3 3-3-1.34-3-3 1.34-3 3-3zm0 14.2a7.2 7.2 0 01-6-3.22c.03-1.99 4-3.08 6-3.08 1.99 0 5.97 1.09 6 3.08a7.2 7.2 0 01-6 3.22z"
+            >
+            </path>
+          </svg>
+          Account
+        </a>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+#mobile_menu {
+    padding-bottom: calc(5px + env(safe-area-inset-bottom));
+}
+</style>
