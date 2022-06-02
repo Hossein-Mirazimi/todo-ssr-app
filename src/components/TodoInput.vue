a1503e018a6eb9734b01998f23fb772a2525af0d
Hossein Mirazimi <h.mirazimi@alibaba.ir>
2022-06-02 22:29:24 +0430

    refactor: seperate components

---
+++ src/components/TodoInput.vue
@@ -0,0 +1,41 @@
+<template>
+  <form class="css-0">
+    <div role="group" class="chakra-form-control css-zn965q">
+      <label
+        id="field-6-label"
+        for="todo"
+        class="chakra-form__label css-vucjec"
+      >
+        <div
+          class="border-primary-card group hover:border-secondary-card rounded-2xl border-3 flex items-center w-full"
+          style="padding: 10px;"
+        >
+          <button
+            class="default-focus text-primary-background text-center flex justify-center items-center mr-3 text-base transition-colors duration-100 ease-in-out rounded-lg"
+            style="height: 22px; width: 23px; border-color: rgb(51, 50, 35); background-color: rgb(51, 50, 35);"
+          >
+            <svg
+              stroke="currentColor"
+              fill="currentColor"
+              stroke-width="0"
+              viewBox="0 0 24 24"
+              height="1em"
+              width="1em"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M19 11h-6V5h-2v6H5v2h6v6h2v-6h6z"></path>
+            </svg>
+          </button>
+          <input
+            id="name"
+            class="w-full bg-transparent rounded-md outline-none"
+            name="name"
+            type="name"
+            placeholder="Add task"
+            required=""
+          >
+        </div>
+      </label>
+    </div>
+  </form>
+</template>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="relative flex items-center justify-start p-3 group rounded-2xl bg-primary-card">
+    <button
+      v-if="!isCompleted"
+      class="default-focus border-3 focus:outline-none bg-opacity-0 hover:bg-opacity-40 px-2 py-2 transition-colors duration-100 ease-in-out rounded-lg"
+      style="border-color: rgb(255, 255, 255);"
+      @click="isCompleted = true"
+    ></button>
+    <button
+      v-else
+      class="default-focus text-primary-background hover:bg-opacity-50 focus:outline-none flex justify-center items-center transition-colors duration-100 ease-in-out rounded-lg"
+      style="height:22px;width:22px;border-color:rgb(255, 255, 51);background-color:rgb(255, 255, 51);"
+      data-v-91ec745a=""
+      @click="isCompleted = false"
+    >
+      <svg
+        stroke="currentColor"
+        fill="currentColor"
+        stroke-width="0"
+        viewBox="0 0 20 20"
+        height="1em"
+        width="1em"
+        xmlns="http://www.w3.org/2000/svg"
+        data-v-91ec745a=""
+      >
+        <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd" data-v-91ec745a=""></path>
+      </svg>
+    </button>
+    <p class="pr-8 ml-3 text-white" :class="{'line-through': isCompleted}">
+      {{ randomTodo }}
+    </p>
+    <div class="focus:outline-none absolute right-0 mr-2 rounded-lg">
+      <div class="relative flex text-left">
+        <button
+          class="default-focus hover:opacity-100 z-10 p-2 transition-opacity duration-200 ease-in-out rounded-lg opacity-50"
+          @click="showMenu = !showMenu"
+        >
+          <svg
+            stroke="currentColor"
+            fill="none"
+            stroke-width="2"
+            viewBox="0 0 24 24"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="text-xl"
+            height="1em"
+            width="1em"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <circle cx="12" cy="12" r="1"></circle>
+            <circle cx="12" cy="5" r="1"></circle>
+            <circle cx="12" cy="19" r="1"></circle>
+          </svg>
+        </button>
+        <div
+          class="origin-top-right absolute right-0 top-5 z-50 mt-2 w-56 rounded-md shadow-lg bg-secondary-background ring-1 ring-black ring-opacity-5 divide-y divide-primary-background"
+          :class="{ 'invisible': !showMenu }"
+          role="menu"
+          aria-orientation="vertical"
+          aria-labelledby="options-menu"
+        >
+          <div v-if="!isCompleted" class="py-1">
+            <button
+              class="default-focus hover:bg-secondary-card flex w-full px-4 py-2 text-sm text-left text-gray-200"
+              role="menuitem"
+            >
+              <div class="default-focus flex flex-row w-full text-left text-white">
+                Edit
+              </div>
+            </button>
+          </div>
+          <div class="py-1">
+            <button
+              class="default-focus hover:bg-secondary-card block w-full px-4 py-2 text-sm text-left text-gray-200"
+              role="menuitem"
+            >
+              Delete
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  props: {
+    isCompleted: {
+      type: Boolean,
+      default: Boolean(Math.floor(Math.random())),
+    },
+  },
+  setup() {
+    const showMenu = ref(false)
+    const todosTitle = ['delectus aut autem', 'fugiat veniam minus', 'laboriosam mollitia et enim quasi adipisci quia provident illum']
+    return {
+      showMenu,
+      randomTodo: todosTitle[Math.floor(Math.random() * todosTitle.length)],
+    }
+  },
+})
+</script>
