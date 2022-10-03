566aedf5cbf1b7f6bb556acf147a9089f3d2cc93
Hossein Mirazimi <h.mirazimi@alibaba.ir>
2022-10-03 22:14:56 +0330

    feat(todo): add collectionId to task model and add update and remove task functions

--- src/store/todo.ts
+++ src/store/todo.ts
@@ -2,6 +2,13 @@ import { defineStore } from 'pinia'
 import { computed, ref } from 'vue'
 import { uuId, sentence, randBool, randomNumberBetween } from '../utils'
 
+export interface Task {
+  collectionId: string
+  id: string
+  name: string
+  isCompleted: boolean
+}
+
 const DEFAULT_COLLECTIONS = [
   {
     id: uuId(),
@@ -13,14 +20,25 @@ const DEFAULT_COLLECTIONS = [
   },
 ]
 
-const DEFAULT_TASKS = DEFAULT_COLLECTIONS.map(({ id: collectionId }) => {
+const DEFAULT_TASKS: Task[] = DEFAULT_COLLECTIONS.map(({ id: collectionId }) => {
   const arrLen = randomNumberBetween(1, 10)
   return Array(arrLen).fill(null).map(() => ({
-    id: collectionId,
+    collectionId,
+    id: uuId(),
     name: sentence(),
     isCompleted: randBool(),
   }))
 }).flat()
+
+const arrLen = randomNumberBetween(1, 10)
+Array(arrLen).fill(null).forEach(() => {
+  DEFAULT_TASKS.push({
+    collectionId: '760bc024b4b7d',
+    id: uuId(),
+    name: sentence(),
+    isCompleted: randBool(),
+  })
+})
 
 export const useTodoStore = defineStore('todo', () => {
   const collections = ref(DEFAULT_COLLECTIONS)
@@ -29,11 +47,46 @@ export const useTodoStore = defineStore('todo', () => {
   const getCollections = computed(() => collections.value)
   const getTasks = computed(() => tasks.value)
 
-  const getTaskFromCollection = (id: string) => getTasks.value?.filter(task => task.id === id)
+  const getTaskFromCollection = (collectionId: string): Task[] => getTasks.value?.filter(task => task.collectionId === collectionId)
+  const getCompletedTaskFromCollection = (collectionId: string): Task[] => getTasks.value?.filter(task => task.collectionId === collectionId && task.isCompleted)
+  const getTodoTaskFromCollection = (collectionId: string): Task[] => getTasks.value?.filter(task => task.collectionId === collectionId && !task.isCompleted)
 
+  const addNewTask = (name: string, collectionId: string): void => {
+    tasks.value.push({
+      collectionId,
+      id: uuId(),
+      name,
+      isCompleted: false,
+    })
+  }
+
+  // @ts-ignore
+  const updateIsCompleteTask = (taskId: string, defaultIsCompleted?: boolean = undefined): void => {
+    const taskIndex = tasks.value.findIndex(task => task.id === taskId)
+    console.log(taskIndex)
+    if (taskIndex >= 0) {
+      const _task = tasks.value[taskIndex]
+      console.log(_task)
+      tasks.value?.splice(taskIndex, 1, {
+        ..._task,
+        isCompleted: defaultIsCompleted !== undefined ? defaultIsCompleted : !_task.isCompleted,
+      })
+      console.log('task updated')
+    }
+  }
+
+  const removeTaskFromCollection = (taskId: string): void => {
+    const taskIndex = tasks.value.findIndex(task => task.id === taskId)
+    taskIndex >= 0 && tasks.value.splice(taskIndex, 1)
+  }
   return {
     getTasks,
     getCollections,
+    addNewTask,
+    updateIsCompleteTask,
+    removeTaskFromCollection,
     getTaskFromCollection,
+    getCompletedTaskFromCollection,
+    getTodoTaskFromCollection,
   }
 })
