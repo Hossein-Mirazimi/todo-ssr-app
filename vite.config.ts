0f0ae03910f51a3dd7031258d2142ef9873cdf7b
Hossein Mirazimi <h.mirazimi@alibaba.ir>
2022-10-08 20:16:54 +0330

    Revert 'feat: add pwa setting'

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { UserConfig } from 'vite'
 import VuePlugin from '@vitejs/plugin-vue'
-import ViteIcons from 'vite-plugin-icons'
-import { VitePWA } from 'vite-plugin-pwa'
+import ViteIcons, { ViteIconsResolver } from 'vite-plugin-icons'
 import Components from 'unplugin-vue-components/vite'
 import WindiCSS from 'vite-plugin-windicss'
 
@@ -23,52 +22,6 @@ const config: UserConfig = {
     ViteIcons({
       scale: 1.1,
       defaultStyle: 'vertical-align: middle;',
-    }),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
-      devOptions: {
-        enabled: process.env.NODE_ENV !== 'production',
-      },
-      manifest: {
-        name: 'Tsk, just tasks',
-        short_name: 'Tsk',
-        theme_color: '#FC76A1',
-        icons: [
-          {
-            src: '/favicon_x16.png',
-            sizes: '16x16',
-            type: 'image/png',
-          },
-          {
-            src: '/favicon_x32.png',
-            sizes: '32x32',
-            type: 'image/png',
-          },
-          /* {
-            src: '/android-chrome-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: '/android-chrome-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-          {
-            src: '/maskable-icon-x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'maskable',
-          },
-          {
-            src: '/maskable-icon-x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-            purpose: 'maskable',
-          }, */
-        ],
-      },
     }),
     WindiCSS(),
   ],
